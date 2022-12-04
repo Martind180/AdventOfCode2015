1b02031d65048829001dffc7aa721d8f34e12d31
Martin Dunn <Martin.dun176@hotmail.com>
2022-12-04 20:37:50 +0000

    Day 1 complete

---
+++ Day1-NotQuiteLisp/NotQuiteLisp/NotQuiteLisp/Program.cs
@@ -0,0 +1,43 @@
+﻿
+var santaDirections = File.ReadAllText("C:\\Users\\marti\\Desktop\\Advent Of Code\\AdventOfCode2015\\Day1-NotQuiteLisp\\SantaDirections.txt");
+
+var answer = 0;
+
+
+foreach (var character in santaDirections)
+{
+    switch (character)
+    {
+        case '(':
+            answer++;
+            break;
+        case ')':
+            answer--;
+            break;
+    }
+}
+
+Console.WriteLine(answer);
+
+//Part 2
+var count = 0;
+
+foreach (var character in santaDirections)
+{
+    if (answer < 0)
+    {
+        Console.WriteLine(count);
+        return;
+    }
+
+    count++;
+    switch (character)
+    {
+        case '(':
+            answer++;
+            break;
+        case ')':
+            answer--;
+            break;
+    }
+}
